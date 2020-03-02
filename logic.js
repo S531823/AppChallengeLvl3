f7e4a1e6a85f89863009490d789dc3b52a00c89c
Tommy Vo <S531823@nwmissouri.edu>
2020-03-02 10:19:21 -0600

    Added changes to html, css, and JS

---
+++ logic.js
@@ -0,0 +1,8 @@
+function buttonClicked() {
+    let firstInput = document.getElementById("firstInput").value;
+    let secondInput = document.getElementById("secondInput").value;
+    let thirdInput = document.getElementById("thirdInput").value;
+    document.getElementById("result1").innerHTML = firstInput;
+    document.getElementById("result2").innerHTML = secondInput;
+    document.getElementById("result3").innerHTML = thirdInput;
+}
