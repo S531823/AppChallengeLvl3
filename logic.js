f361402f8998f830552c36d5580a3db99c1b0ba6
Tommy Vo <S531823@nwmissouri.edu>
2020-03-02 10:25:46 -0600

    Completed calculation

--- logic.js
+++ logic.js
@@ -2,7 +2,12 @@ function buttonClicked() {
     let firstInput = document.getElementById("firstInput").value;
     let secondInput = document.getElementById("secondInput").value;
     let thirdInput = document.getElementById("thirdInput").value;
+    
+    let result = firstInput * secondInput * thirdInput;
+    
     document.getElementById("result1").innerHTML = firstInput;
     document.getElementById("result2").innerHTML = secondInput;
     document.getElementById("result3").innerHTML = thirdInput;
+    
+    document.getElementById("volume").innerHTML = "Volume: " + result;
 }
